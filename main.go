69bad881e5b701b32023dfc0bec321cfa3c29900
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-10-17 05:19:23 -0500

    main.go now prints message

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"crabot/crabtalk"
 )
 
 func main() {
-	crabtalk.Talk("Hello World")
+	// Convert message to morse and print it
+	message, err := crabtalk.Talk("Hello World")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(message)
 }
