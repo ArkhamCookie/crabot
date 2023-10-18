9beae8b26169d78dc6d82a1b011852b5d185c57f
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-10-18 01:57:45 -0500

    Created `lib/formatting/markdown.go`
    Transforms text into markdown syntax
    based on given style

---
+++ lib/formatting/markdown.go
@@ -0,0 +1,35 @@
+package formatting
+
+import (
+	"errors"
+)
+
+// Discord's markdown syntax
+var discordMarkdownData = map[string]string{
+	"italic":     "*",
+	"bold":       "**",
+	"boldItalic": "***",
+
+	"strikethough": "~~",
+}
+
+func DiscordMarkdown(text, style string) (string, error) {
+	// Confirm that text is given
+	if text == "" {
+		return "", errors.New("no text given")
+	}
+	// Confirm that style type is given
+	if style == "" {
+		return "", errors.New("no style given")
+	}
+	// Confirm style type exists
+	_, ok := discordMarkdownData[style]
+	if !ok {
+		return "", errors.New("given style type doesn't exist")
+	}
+
+	// Transform based on style
+	output := discordMarkdownData[style] + text + discordMarkdownData[style]
+
+	return output, nil
+}
