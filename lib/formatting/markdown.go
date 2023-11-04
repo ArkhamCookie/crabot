b6f557f85973a7afe918cb517f98af1e578c499e
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-11-03 23:47:22 -0500

    Add comments + start to move blockquote

--- lib/formatting/markdown.go
+++ lib/formatting/markdown.go
@@ -1,9 +1,13 @@
 package formatting
 
-// Surround Markdown
+// Format input into italic markdown style
 func Italic(input string) string       { return "*" + input + "*" }
-func Bold(input string) string         { return "**" + input + "**" }
-func BoldItalic(input string) string   { return "***" + input + "***" }
-func Strikethough(input string) string { return "~~" + input + "~~" }
 
-func BlockQuote(input string) string { return "> " + input }
+// Format input into Bold markdown style
+func Bold(input string) string         { return "**" + input + "**" }
+
+// Format input into Bold+Italic markdown style
+func BoldItalic(input string) string   { return "***" + input + "***" }
+
+// Format input into Strikethough markdown style
+func Strikethough(input string) string { return "~~" + input + "~~" }
