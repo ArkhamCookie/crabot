93eb3131f806faead6fe16aef50818bf34b58b4e
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-11-03 23:28:16 -0500

    Simplified markdown even more
    At first I thought having the surround function would be useful,
    in a DRY sort of way,
    but I think it was just making the code more complicated for no reason

--- lib/formatting/markdown.go
+++ lib/formatting/markdown.go
@@ -1,11 +1,9 @@
 package formatting
 
-func surround(markdownData string, text string) string { return markdownData + text + markdownData }
-
 // Surround Markdown
-func Italic(input string) string       { return surround("*", input) }
-func Bold(input string) string         { return surround("**", input) }
-func BoldItalic(input string) string   { return surround("***", input) }
-func Strikethough(input string) string { return surround("~~", input) }
+func Italic(input string) string       { return "*" + input + "*" }
+func Bold(input string) string         { return "**" + input + "**" }
+func BoldItalic(input string) string   { return "***" + input + "***" }
+func Strikethough(input string) string { return "~~" + input + "~~" }
 
 func BlockQuote(input string) string { return "> " + input }
