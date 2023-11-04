8fd01a184e9e86c2242b0eb4b5fd11cf16792b53
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-11-03 23:50:02 -0500

    Test basic markdown

--- lib/formatting/formatting_test.go
+++ lib/formatting/formatting_test.go
@@ -1,19 +1,68 @@
 package formatting_test
 
 import (
-	"regexp"
 	"testing"
 
 	"crabot/formatting"
 )
 
-func TestDiscordMarkdown(t *testing.T) {
-	input := "crabot/formatting test!"
-	style := "italic"
-	want := regexp.MustCompile(`\*` + input + `\*`)
+var (
+	input string
+	want string
+	output string
+)
 
-	output, err := formatting.DiscordMarkdown(input, style)
-	if !want.MatchString(output) || err != nil {
-		t.Fatalf(`DiscordItalics("crabot/formatting test!", "italic") = %q, %v, want match for %#q, nil`, output, err, want)
+// Italic Test
+func TestMarkdownItalic(t *testing.T) {
+	input = "crabot/formatting italic test!"
+	want = "*" + input + "*"
+
+	output = formatting.Italic(input)
+	if want != output {
+		t.Fatalf(`formatting.Italic(%q) = %q, want match for %#q`, input, output, want)
 	}
 }
+
+// Bold Test
+func TestMarkdownBold(t *testing.T) {
+	input = "crabot/formatting bold test!"
+	want = "**" + input + "**"
+
+	output = formatting.Bold(input)
+	if want != output {
+		t.Fatalf(`formatting.Bold(%q) = %q, want match for %#q`, input, output, want)
+	}
+}
+
+func TestMarkdownBoldItalic(t *testing.T) {
+	input = "crabot/formatting bold italic test!"
+	want = "***" + input + "***"
+
+	output = formatting.BoldItalic(input)
+	if want != output {
+		t.Fatalf(`formatting.BoldItalic(%q) = %q, want match for %#q`, input, output, want)
+	}
+}
+
+func TestMarkdownStrikethough(t *testing.T) {
+	input = "crabot/formatting strikethough test!"
+	want = "~~" + input + "~~"
+
+	output = formatting.Strikethough(input)
+	if want != output {
+		t.Fatalf(`formatting.Strikethough(%q) = %q, want match for %#q`, input, output, want)
+	}
+}
+
+// TestMarkdown Template
+/*
+func TestMarkdownFoo(t *testing.T) {
+	input = "crabot/formatting foo test!"
+	want = input
+
+	output = formatting.Foo(input)
+	if want != output {
+		t.Fatalf(`formatting.Foo(%q) = %q, want match for %#q`, input, output, want)
+	}
+}
+*/
