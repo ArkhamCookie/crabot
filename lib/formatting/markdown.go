2ee995cea94b066bbe259ac9f2e5526c71105a91
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-11-03 23:47:44 -0500

    Run gofmt

--- lib/formatting/markdown.go
+++ lib/formatting/markdown.go
@@ -1,13 +1,13 @@
 package formatting
 
 // Format input into italic markdown style
-func Italic(input string) string       { return "*" + input + "*" }
+func Italic(input string) string { return "*" + input + "*" }
 
 // Format input into Bold markdown style
-func Bold(input string) string         { return "**" + input + "**" }
+func Bold(input string) string { return "**" + input + "**" }
 
 // Format input into Bold+Italic markdown style
-func BoldItalic(input string) string   { return "***" + input + "***" }
+func BoldItalic(input string) string { return "***" + input + "***" }
 
 // Format input into Strikethough markdown style
 func Strikethough(input string) string { return "~~" + input + "~~" }
