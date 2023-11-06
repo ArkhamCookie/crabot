3aa7a1239a83a7ef82f01fc7a00d9280af0e97dc
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-11-06 07:36:38 -0600

    Created TestGetEnvValue

---
+++ internal/env/env_test.go
@@ -0,0 +1,31 @@
+package env_test
+
+import (
+	"testing"
+
+	"internal/env"
+)
+
+var (
+	err error
+	// input string
+	output string
+	want string
+)
+
+func TestGetEnvValue(t *testing.T) {
+	inputValue := "GET_TEST"
+	inputFile := ".env.test"
+	want = "load-test-successful"
+
+	output, err = env.GetEnvValue(inputValue, inputFile)
+	if err != nil {
+		t.Fatal("Error:", err)
+	}
+	if want != output {
+		t.Fatalf(
+			`env.GetEnvValue(%q, %q) = %q, want match for %#q`,
+			inputValue, inputFile, output, want,
+		)
+	}
+}
