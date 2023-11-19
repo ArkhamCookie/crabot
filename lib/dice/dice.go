c4169894c9e3df2b00d68be4d77c32b99b2159ef
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-11-18 18:43:30 -0600

    gofmt

--- lib/dice/dice.go
+++ lib/dice/dice.go
@@ -3,6 +3,6 @@ package dice
 import "internal/dice"
 
 // D2 rolls a D2 (1-2)
-func D2() int{
+func D2() int {
 	return dice.Roll(2)
 }
--- lib/dice/internal/roll.go
+++ lib/dice/internal/roll.go
@@ -3,7 +3,7 @@ package dice
 import "math/rand"
 
 // Roll *dice* and return the roll.
-func Roll(dice int) (int) {
+func Roll(dice int) int {
 	// Add 1 so it 1-int rather than 0-int
 	return rand.Intn(dice) + 1
 }
