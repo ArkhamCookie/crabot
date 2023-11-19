da7153b2371061378131d8889f30a6ff8adbc25d
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-11-19 00:38:11 -0600

    Created first test for diceTypes

--- lib/dice/diceTypes_test.go
+++ lib/dice/diceTypes_test.go
@@ -1,1 +1,25 @@
 package dice_test
+
+import (
+	"testing"
+
+	"crabot/dice"
+)
+
+var (
+	result int
+)
+
+func TestD2(t *testing.T) {
+	result = dice.D6(1)
+
+	// TODO: Test that it is a full number
+
+	if result < 0 {
+		t.Fatal("dice.D6 was negitive")
+	} else if result == 0 {
+		t.Fatal("dice.D6 was 0")
+	} else if result > 6 {
+		t.Fatal("dice.D6 was greater than 6")
+	}
+}
