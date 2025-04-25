986dc8e9170006446cb59095b02ac264a2cc2de4
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-04-24 22:11:21 -0500

    feat: add D8 tests

--- lib/dice/diceTypes_test.go
+++ lib/dice/diceTypes_test.go
@@ -94,6 +94,34 @@ func TestD6(t *testing.T) {
 	}
 }
 
+func TestD8(t *testing.T) {
+	result = dice.D8(1)
+
+	if result < 0 {
+		t.Fatal("dice.D8(1) was negative")
+	}
+
+	if result == 0 {
+		t.Fatal("dice.D8(1) was 0")
+	}
+
+	if result > 2 {
+		t.Fatal("dice.D8(1) was greater than 2")
+	}
+
+	result = dice.D8(2)
+
+	if result < 0 {
+		t.Fatal("dice.D8(2) was negative")
+	} else if result == 0 {
+		t.Fatal("dice.D8(2) was 0")
+	}
+
+	if result < 2 {
+		t.Fatal("dice.D8(2) was less than 2")
+	}
+}
+
 func TestD00(t *testing.T) {
 	percentileResults := dice.D00(1)
 
