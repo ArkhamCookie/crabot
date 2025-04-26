862a990a8a3e6338b3284e38e388c8342d622917
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-04-26 12:25:46 -0500

    feat: don't remove commands on shutdown by default

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 var (
 	GuildID        = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
 	BotToken       = flag.String("token", "", "Bot access token")
-	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
+	RemoveCommands = flag.Bool("rmcmd", false, "Remove all commands after shutdowning or not")
 )
 
 // Session
