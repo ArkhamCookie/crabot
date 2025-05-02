547eced4f8c5bb5209b46a1f35981815a5153d24
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-05-02 14:55:10 -0500

    feat: create `GetUploadsPlaylistID` func

--- lib/lastupload/types.go
+++ lib/lastupload/types.go
@@ -41,3 +41,23 @@ type ChannelSearch struct {
 		} `json:"snippet"`
 	} `json:"items"`
 }
+
+type UploadsPlaylistSearch struct {
+	Kind     string `json:"kind"`
+	Etag     string `json:"etag"`
+	PageInfo struct {
+		TotalResults   int `json:"totalResults"`
+		ResultsPerPage int `json:"resultsPerPage"`
+	} `json:"pageInfo"`
+	Items []struct {
+		Kind           string `json:"kind"`
+		Etag           string `json:"etag"`
+		ID             string `json:"id"`
+		ContentDetails struct {
+			RelatedPlaylists struct {
+				Likes   string `json:"likes"`
+				Uploads string `json:"uploads"`
+			} `json:"relatedPlaylists"`
+		} `json:"contentDetails"`
+	} `json:"items"`
+}
