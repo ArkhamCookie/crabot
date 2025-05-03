811effa042c30f9d8b5196135c207778752a3c0b
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-05-03 14:16:49 -0500

    feat: create GetDate func for lastupload

--- lib/lastupload/get.go
+++ lib/lastupload/get.go
@@ -1,5 +1,10 @@
 package lastupload
 
+import (
+	"regexp"
+	"time"
+)
+
 func Get(username, YOUTUBE_KEY string) ([]LastUploadResults, error) {
 	channelID, err := GetChannelID(username, YOUTUBE_KEY)
 	if err != nil {
@@ -17,4 +22,47 @@ func Get(username, YOUTUBE_KEY string) ([]LastUploadResults, error) {
 	}
 
 	return lastUploadData, nil
+}
+
+func GetDate(lastUploadResults []LastUploadResults) (time.Time, error) {
+	// Get last upload date from lastUploadResults
+	lastUpload := lastUploadResults[0].Snippet.PublishedAt.String()
+
+	regex, err := regexp.Compile("T")
+	if err != nil {
+		return time.Now(), err
+	}
+
+	// Replace the **SECOND** 'T' in lastUpload
+	count := 0
+	reformatted := regex.ReplaceAllStringFunc(lastUpload, func(s string) string {
+		count++
+		if count == 2 {
+			return " "
+		}
+		return s
+	})
+
+	regex, err = regexp.Compile("Z")
+	if err != nil {
+		return time.Now(), err
+	}
+
+	// Replace all 'Z's in reformatted
+	reformatted = regex.ReplaceAllString(reformatted, " ")
+
+	regex, err = regexp.Compile(" .0000 UTC")
+	if err != nil {
+		return time.Now(), err
+	}
+
+	reformatted = regex.ReplaceAllString(reformatted, "")
+
+	layout := "2006-01-02 15:04:05"
+	uploadTime, err := time.Parse(layout, reformatted)
+	if err != nil {
+		return time.Now(), err
+	}
+
+	return uploadTime, nil
 }
