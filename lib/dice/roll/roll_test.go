f9a1a705ed69dc9b1b5db7898f328ea6c07465be
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-05-04 18:27:31 -0500

    feat: basic test for roll func

---
+++ lib/dice/roll/roll_test.go
@@ -0,0 +1,15 @@
+package roll_test
+
+import (
+	"testing"
+
+	"crabot/dice/roll"
+)
+
+func TestRoll(t *testing.T) {
+	output := roll.Roll(1)
+
+	if output != 1 {
+		t.Fatalf("`roll.Roll(1)` = %q, wanted 1", output)
+	}
+}
