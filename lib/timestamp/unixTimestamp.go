16aa862eca13e446d66ce975f3bf643c68d5378b
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-05-07 17:07:31 -0500

    docs: add doc comments for `unixTimestamps.go`

--- lib/timestamp/unixTimestamp.go
+++ lib/timestamp/unixTimestamp.go
@@ -4,30 +4,37 @@ import (
 	"fmt"
 )
 
+// Get timestamp from unix timestamp in short date format
 func UnixShortDate(unixTime int64) string {
 	return fmt.Sprintf("<t:%v:d>", unixTime)
 }
 
+// Get timestamp from unix timestamp in long date format
 func UnixLongDate(unixTime int64) string {
 	return fmt.Sprintf("<t:%v:D>", unixTime)
 }
 
+// Get timestamp from unix timestamp in short time format
 func UnixShortTime(unixTime int64) string {
 	return fmt.Sprintf("<t:%v:t>", unixTime)
 }
 
+// Get timestamp from unix timestamp in long time format
 func UnixLongTime(unixTime int64) string {
 	return fmt.Sprintf("<t:%v:T>", unixTime)
 }
 
+// Get timestamp from unix timestamp in short full format
 func UnixShortFull(unixTime int64) string {
 	return fmt.Sprintf("<t:%v:f>", unixTime)
 }
 
+// Get timestamp from unix timestamp in long full format
 func UnixLongFull(unixTime int64) string {
 	return fmt.Sprintf("<t:%v:F>", unixTime)
 }
 
+// Get timestamp from unix timestamp in relative time format
 func UnixRelativeTime(unixTime int64) string {
 	return fmt.Sprintf("<t:%v:R>", unixTime)
 }
